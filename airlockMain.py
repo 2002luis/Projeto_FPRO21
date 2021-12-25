97a6d14cf168234865a6834680f6a8d6da3790f5
Luís Sousa <luis.m.sousa10@gmail.com>
2021-12-25 14:27:40 +0000

    Sons (script)
    Código relacionado ao som

--- airlockMain.py
+++ airlockMain.py
@@ -80,7 +80,7 @@ def readFile(strPath):
 |_|_| |_|_|
 '''
 
-
+pygame.mixer.init() ##MUSICA E SONS
 pygame.init()
 pygame.display.set_caption("Airlock da Wish")
 icon=pygame.image.load("images/icon.png")
@@ -134,8 +134,29 @@ playerFallenImage=pygame.image.load("images/playerCharHurt.png")
 bigSwitchImage=pygame.image.load("images/bigSwitch.png")
 smallSwitchImage=pygame.image.load("images/smallSwitch.png")
 enemyImage=pygame.image.load("images/enemyChar.png")
-mainMenu=pygame.image.load("images/mainMenu.png")
+mainMenuImage=pygame.image.load("images/mainMenu.png")
+victoryRoyaleImage=pygame.image.load("images/victoryRoyale.png")
+gameOverImage=pygame.image.load("images/gameOver.png")
 peixes=[pygame.image.load("images/peixe1.png"),pygame.image.load("images/peixe2.png"),pygame.image.load("images/peixe3.png"),pygame.image.load("images/inimigoDoJCL.png")]
+
+##SONS
+buttonSound = pygame.mixer.Sound("sound/button.ogg")
+elevatorSound = pygame.mixer.Sound("sound/elevator.ogg")
+enemyHitSound = pygame.mixer.Sound("sound/enemyHit.ogg")
+epicWinSound = pygame.mixer.Sound("sound/epicWin.ogg")
+gameOverSound = pygame.mixer.Sound("sound/gameOver.ogg")
+jumpSound = pygame.mixer.Sound("sound/jump.ogg")
+menuMusicSound = pygame.mixer.Sound("sound/menuMusic.ogg")
+walkSound = pygame.mixer.Sound("sound/walk.ogg")
+wallSound = pygame.mixer.Sound("sound/wall.ogg")
+victoryRoyaleSound = pygame.mixer.Sound("sound/epicWin.ogg")
+gameOverSound = pygame.mixer.Sound("sound/gameOver.ogg")
+
+##CHANNELS DE SOM
+jumpChannel = pygame.mixer.Channel(0)
+walkChannel = pygame.mixer.Channel(1)
+mainMenuChannel = pygame.mixer.Channel(2)
+gameOverChannel = pygame.mixer.Channel(3)
 
 ##ENQUANTO NAO TIVER UM ECRA DE TITULO VOU INICIALIZAR TUDO AQUI
 for wall in walls:
@@ -159,39 +180,49 @@ player.boolFloor=False
 boolDebugger=False
 boolMainMenu=True
 boolTempImmunity=True ##O JOGO FICA PARADO ATÉ AO PRIMEIRO MOVIMENTO (COMO NO ORIGINAL)
+boolVictoryRoyale=False ##SÓ VAI IMPORTAR DEPOIS DE ACABAR O PRIMEIRO JOGO
 intTimeLeft=49999
 timeMainMenu=200
 
 while(not kill):
     if(boolMainMenu): ##MENU!
-        dt=clock.tick()
-        '''
-        EVENTOS
-        '''
-        boolTempImmunity=True
-        if(timeMainMenu>0):
-            timeMainMenu-=dt
-        events = pygame.event.get()
-        for event in events:
-            if(event.type == pygame.KEYDOWN and timeMainMenu<=0):
-                boolMainMenu=False
-            elif(event.type == pygame.QUIT):
-                kill=True
+        if(not gameOverChannel.get_busy()):
+            dt=clock.tick()
+            if(not mainMenuChannel.get_busy()):
+                mainMenuChannel.play(menuMusicSound)
+            '''
+            EVENTOS
+            '''
+            boolTempImmunity=True
+            if(timeMainMenu>0):
+                timeMainMenu-=dt
+            events = pygame.event.get()
+            for event in events:
+                if(event.type == pygame.KEYDOWN and timeMainMenu<=0):
+                    boolMainMenu=False
+                elif(event.type == pygame.QUIT):
+                    kill=True
 
-        '''
-        FÍSICA (peixinhos!)
-        '''
+            '''
+            "FÍSICA" (peixinhos!)
+            '''
 
-        moveFish(fishes,dt)
+            moveFish(fishes,dt)
 
-        '''
-        DESENHAR
-        '''
-        pygame.draw.rect(screen, (0,0,0), pygame.Rect(0, 0, 800, 600))
-        screen.blit(mainMenu,(0,0))
-        for i in range(len(fishes)):
-            for j in range(len(fishes[i])):
-                fishes[i][j].Draw(screen,peixes[i])
+            '''
+            DESENHAR
+            '''
+            pygame.draw.rect(screen, (0,0,0), pygame.Rect(0, 0, 800, 600))
+            screen.blit(mainMenuImage,(0,0))
+            for i in range(len(fishes)):
+                for j in range(len(fishes[i])):
+                    fishes[i][j].Draw(screen,peixes[i])
+        else:
+            if(boolVictoryRoyale):
+                screen.blit(victoryRoyaleImage, (0, 0))
+            else:
+                screen.blit(gameOverImage, (0, 0))
+            ##O PYGAME.DISPLAY.FLIP() ESTÁ NO FIM DE TUDO, FORA DOS IFS E ELSES
     else:
         '''
         EVENTOS
@@ -210,6 +241,7 @@ while(not kill):
                     boolTempImmunity = False
                     boolHoldingW = True
                     player.Jump()
+                    jumpChannel.play(jumpSound)
         if (keys[pygame.K_a] and (not player.boolJumping)):
             boolTempImmunity = False
             intDir=-1
@@ -251,6 +283,7 @@ while(not kill):
             if(badCollision(player,enemies,enemyImage) and not boolTempImmunity): ##DETETAR SE BATEU EM UM INIMGO
                 player.intHurt=3000 ##3 SEGUNDOS
                 player.floatY=player.intFloorY
+                enemyHitSound.play()
             else:
                 if(not(elevators[intCurFloor].boolElevating)): ##SO SE MEXE SE NAO ESTIVER EM UM ELEVADOR
                     intNewDir=player.HandleBlocks(badCollision(player,blocks,blockImage),intDir)
@@ -259,7 +292,11 @@ while(not kill):
                         intNoMoreJumping=50
                         player.floatVy=0.3
                         intDir=intNewDir
+                        wallSound.play()
+                    elif(intDir!=0 and player.floatY==player.intFloorY and not walkChannel.get_busy()):
+                        walkChannel.play(walkSound)
                     player.Move(intDir,dt,walls[intCurFloor].boolEnabled)
+                    
                 player.Fall(dt)
                 if(player.floatY>=player.intFloorY and player.floatVy>0):
                     player.floatY=player.intFloorY
@@ -270,11 +307,13 @@ while(not kill):
             
             if(bigSwitches[intCurFloor].boolEnabled and goodCollision(player,bigSwitches[intCurFloor],bigSwitchImage)): ##DETETAR SE CARREGOU EM UM BOTAO GRANDE
                 bigSwitches[intCurFloor].press(walls)
+                buttonSound.play()
             
             elif(goodCollision(player,smallSwitches[intCurFloor],smallSwitchImage) and not bigSwitches[intCurFloor].boolEnabled and smallSwitches[intCurFloor].boolEnabled): ##DETETAR SE CARREGOU EM UM BOTAO PEQUENO
                 smallSwitches[intCurFloor].press(walls[intCurFloor])
                 if(intCurFloor<4):
                     walls[intCurFloor+1].boolEnabled=False
+                buttonSound.play()
             
             elif(not elevators[intCurFloor].boolEnabled): ##DETETAR SE O ELEVADOR ACABOU DE SUBIR
                 intCurFloor+=1
@@ -282,7 +321,10 @@ while(not kill):
                     boolMainMenu = True
                     intTimeLeft = 49999
                     timeMainMenu = 1000
+                    gameOverChannel.play(victoryRoyaleSound)
+                    boolVictoryRoyale=True
                     ##RESET AS VARIAVEIS PARA UM JOGO NOVO
+                    fishes = [[fish(175,310,1,0.15)],[fish(200,404,-1,0.23),fish(300,404,-1,0.23),fish(400,404,-1,0.23)],[fish(600,500,1,0.05)],[fish(100,586,-1,0.005),fish(300,586,-1,0.005),fish(500,586,-1,0.005),fish(700,586,-1,0.005),fish(-26,586,-1,0.005)]]
                     intDir=0
                     intCurFloor=0 ##O PISO DE BAIXO E 0, O DE CIMA DE TUDO E O 4
                     intNoMoreJumping=0 ##VARIAVEL PARA PREVENIR PYGAME BRUH MOMENTS
@@ -309,7 +351,38 @@ while(not kill):
             elif(not bigSwitches[intCurFloor].boolEnabled and not smallSwitches[intCurFloor].boolEnabled): ##DETETAR SE ESTÁ EM UM ELEVADOR
                 if(elevators[intCurFloor].IsElevator(player) or elevators[intCurFloor].boolElevating):
                     elevators[intCurFloor].Elevate(player,floors[intCurFloor+1],dt)
+                    elevatorSound.play()
 
+            if(4-(intTimeLeft//10000)>intCurFloor): ##SE A ÁGUA TIVER SUBIDO PARA CIMA DELE E ELE MORRER
+                boolMainMenu = True
+                intTimeLeft = 49999
+                timeMainMenu = 1000
+                gameOverChannel.play(gameOverSound)
+                boolVictoryRoyale=False
+                ##RESET AS VARIAVEIS PARA UM JOGO NOVO
+                fishes = [[fish(175,310,1,0.15)],[fish(200,404,-1,0.23),fish(300,404,-1,0.23),fish(400,404,-1,0.23)],[fish(600,500,1,0.05)],[fish(100,586,-1,0.005),fish(300,586,-1,0.005),fish(500,586,-1,0.005),fish(700,586,-1,0.005),fish(-26,586,-1,0.005)]]
+                intDir=0
+                intCurFloor=0 ##O PISO DE BAIXO E 0, O DE CIMA DE TUDO E O 4
+                intNoMoreJumping=0 ##VARIAVEL PARA PREVENIR PYGAME BRUH MOMENTS
+                boolHoldingW=False
+                for wall in walls:
+                    wall.boolEnabled=True
+                for i in range(len(elevators)):
+                    elevators[i].boolEnabled=True
+                    elevators[i].boolElevating=False
+                    elevators[i].intY=floors[i]
+                for block in blocks:
+                    block.boolEnabled=True
+                for switch in bigSwitches:
+                    switch.boolEnabled=True
+                for switch in smallSwitches:
+                    switch.boolEnabled=True
+                for enemy in enemies:
+                    enemy.boolEnabled=True
+                player.boolEnabled=True
+                player.boolFloor=False
+                player=playerClass()
+                enemies=data[0] ##PARA OS INIMIGOS VOLTAREM AS POSIÇOES INICIAIS
 
 
         ##MEXER OS INIMIGOS
@@ -323,7 +396,8 @@ while(not kill):
         ##screen.fill((255,255,255)) EU SEI QUE O .fill EXISTE, MAS PREFIRO FAZER ISTO ASSIM
         pygame.draw.rect(screen, (0,0,0), pygame.Rect(0, 0, 800, 600))
         pygame.draw.rect(screen, (0,0,255), pygame.Rect(0, floors[4-(intTimeLeft//10000)], 800, 600))
-        print(intTimeLeft)
+        
+
         screen.blit(levelImage, (0,0))
 
         drawList(walls,screen,wallImage)
